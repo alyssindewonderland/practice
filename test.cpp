317e660280bac02f96498c70c4ea8386528299f3
alyssindewonderland <alyss.vd@gmail.com>
2026-03-19 12:41:37 +0100

    add two numbers done

--- test.cpp
+++ test.cpp
@@ -8,14 +8,47 @@ struct ListNode {
 };
 
 class Solution {
-    public:
-        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-            int unit = 0;
-            int res1 = l1;
-            int res2 = l2;
-            while (l1 != nullptr || l2 != nullptr) {
-                ListNode l1 = l1->next;
-                int temp = l1 * 10**unit;
-            }
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int x = (l1 ? l1->val : 0), y = (l2 ? l2->val : 0);
+            int num = x + y + carry;
+            int remain = num % 10;
+            
+            carry = num / 10;
+            
+            current->next = new ListNode(remain);
+            current = current->next;
+
+            if (l1 != nullptr) l1 = l1->next;
+            if (l2 != nullptr) l2 = l2->next;
         }
+
+        return dummy->next;
+    }
 };
+
+int main() {
+    ListNode* a = new ListNode(2);
+    a->next = new ListNode(4);
+    a->next->next = new ListNode(3);
+
+    ListNode* b = new ListNode(5);
+    b->next = new ListNode(6);
+    b->next->next = new ListNode(4);
+
+    Solution s;
+    ListNode* result = s.addTwoNumbers(a, b);
+
+    while (result != nullptr) {
+        cout << result->val << " ";
+        result = result->next;
+    }
+    cout << "\n";
+
+    return 0;
+}
