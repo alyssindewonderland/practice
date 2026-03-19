417a8f7d54dbacb6a07619d699b80ff03f6ced04
alyssindewonderland <alyss.vd@gmail.com>
2026-03-19 10:20:03 +0100

    uhh

--- test.cpp
+++ test.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
-            }
-        }
-        return {};
-    }
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
 };
 
-int main() {
-    Solution s;
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
-
-    vector<int> ans = s.twoSum(nums, target);
-    for (int x : ans) cout << x << " ";
-    cout << endl;
-}
+class Solution {
+    public:
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+            int unit = 0;
+            int res1 = l1;
+            int res2 = l2;
+            while (l1 != nullptr || l2 != nullptr) {
+                ListNode l1 = l1->next;
+                int temp = l1 * 10**unit;
+            }
+        }
+};
